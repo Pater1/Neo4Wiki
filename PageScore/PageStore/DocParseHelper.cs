60ac12f25c84b8d84b0bb2aa275217522d5aa674
Patrick Conner <PConner000@gmail.com>
2017-08-16 13:26:30 -0600

    DocParseHelper.cs      Counts each word in text from XML, Correct-grammer text, and normalized text.      Untested

---
+++ PageScore/PageStore/DocParseHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace PageScore
+{
+    public static class DocParseHelper
+    {
+        public static Dictionary<string, int> GetWordCountDictFromXMLString(this string xml)
+        {
+            return xml.GetXMLContentRaw().GetTextContentRaw().CountTextWords();
+        }
+
+        public static string GetXMLContentRaw(this string xml)
+        {
+            string ret = "";
+            XmlReader rdr = XmlReader.Create(xml);
+            while (rdr.Read())
+            {
+                ret += rdr.ReadInnerXml();
+            }
+            return ret;
+        }
+
+        public static string GetTextContentRaw(this string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            return rgx.Replace(text, "").ToLower();
+        }
+
+        public static Dictionary<string, int> CountTextWords(this string normalized)
+        {
+            Dictionary<string, int> ret = new Dictionary<string, int>();
+            string[] strs = normalized.Split(' ');
+            foreach(string st in strs)
+            {
+                if (ret.ContainsKey(st))
+                {
+                    ret.Add(st, 1);
+                }
+                else
+                {
+                    ret[st]++;
+                }
+            }
+            return ret;
+        }
+    }
+}
