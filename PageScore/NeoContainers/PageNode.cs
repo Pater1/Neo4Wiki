7f0503d0e4ff783e6e23935d2ce9d552623be886
Patrick Conner <PConner000@gmail.com>
2017-08-17 13:20:37 -0600

    PageNode.cs Done!

---
+++ PageScore/NeoContainers/PageNode.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using driver = Neo4j.Driver.V1;
+using client = Neo4jClient;
+using PageScore;
+
+namespace NeoContainers
+{
+    public struct PageNode: driver.INode
+    {
+        public static PageNode BuildFromINode(driver.INode node)
+        {
+            return new PageNode(
+                (long)node.Properties["ID"],
+                (string)node.Properties["Title"],
+                (string)node.Properties["Text"],
+                (string[])node.Properties["WordList"],
+                (int[])node.Properties["WordCount"]
+            );
+        }
+        public void WriteToDatabase(client.GraphClient graphClient)
+        {
+            graphClient.Connect();
+
+            using (client.Transactions.ITransaction trans = graphClient.BeginTransaction())
+            {
+                graphClient.Cypher.Create($"(n:Page {LabelsAsString})").WithParams(this).ExecuteWithoutResults();
+
+                trans.Commit();
+            }
+        }
+
+        public long ID { get; }
+        public string Title { get; }
+        public string Text { get; }
+        public Dictionary<string, int> WordCountDict { get; }
+        private string[] WordList => WordCountDict.Select(x => x.Key).ToArray();
+        private int[] WordCount => WordCountDict.Select(x => x.Value).ToArray();
+
+        public PageNode(long iD, string title, string text) : this()
+        {
+            ID = iD;
+            Title = title;
+            Text = text;
+            WordCountDict = text.GetWordCountDictFromXMLString();
+        }
+        public PageNode(long iD, string title, string text, string[] words, int[] count) : this()
+        {
+            ID = iD;
+            Title = title;
+            Text = text;
+            WordCountDict = ZipTogetherArrays(words, count);
+        }
+
+        private static Dictionary<string, int> ZipTogetherArrays(string[] words, int[] count)
+        {
+            Dictionary <string, int> ret = new Dictionary<string, int>();
+            for(int i = 0; i < words.Length; i++)
+            {
+                ret.Add(words[i], count[i]);
+            }
+            return ret;
+        }
+
+        #region INode
+        public object this[string key] => Properties[key];
+
+        public IReadOnlyList<string> Labels => Properties.ToList().Select(x => x.Key).ToList();
+        public string LabelsAsString
+        {
+            get
+            {
+                string ret = "{";
+                IReadOnlyList<string> lbls = Labels;
+                bool first = true;
+                foreach(string s in lbls)
+                {
+                    ret += (first? "": ", ") + s + ": {" + s + "}";
+                    first = false;
+                }
+                ret += "}";
+                Console.WriteLine(ret);
+                return ret;
+            }
+        }
+
+        private Dictionary<string, object> properties;
+        public IReadOnlyDictionary<string, object> Properties
+        {
+            get
+            {
+                if(properties == null)
+                {
+                    properties = new Dictionary<string, object>();
+                    properties.Add("ID", ID);
+                    properties.Add("Title", Title);
+                    properties.Add("Text", Text);
+                    properties.Add("WordList", WordList);
+                    properties.Add("WordCount", WordCount);
+                }
+                else
+                {
+                    properties["ID"] = ID;
+                    properties["Title"] = Title;
+                    properties["Text"] = Text;
+                    properties["WordList"] = WordCountDict.Select(x => x.Key).ToArray();
+                    properties["WordCount"] = WordCountDict.Select(x => x.Value).ToArray();
+                }
+                return properties;
+            }
+        }
+
+        public long Id => ID;
+        
+        public bool Equals(driver.INode other)
+        {
+            if (other.GetType() != typeof(PageNode)) return false;
+
+            PageNode pn = (PageNode)other;
+            if (pn.ID != ID) return false;
+            if (pn.Title != Title) return false;
+
+            return true;
+        }
+        #endregion
+    }
+}
--- PageScore/PageScorePOC/Program.cs
+++ PageScore/PageScorePOC/Program.cs
@@ -4,23 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PageScore;
+using NeoContainers;
+using driver = Neo4j.Driver.V1;
+using client = Neo4jClient;
 
 namespace PageScorePOC
 {
     class Program
     {
-        static string xml = @"
+        static string xml1 = @"
             <body>
                 <h1>Hello World</h1>
                 <p>This is a test!</p>
                 <p>oUEfnoue <br/>OEifE9f23jrr2 24840 4810 ---</p>
             </body>";
+        static string xml2 = @"
+            <body>
+                <h1>How Goes?</h1>
+            </body>";
+        static string xml3 = @"
+            <body>
+                <p>This is a test!</p>
+                <p>Or... Is it?!?</p>
+            </body>";
         static void Main(string[] args)
         {
-            Dictionary<string, int> cnt = xml.GetWordCountDictFromXMLString();
-            foreach(KeyValuePair<string, int> p in cnt)
+            client.GraphClient graphClient = new client.GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "password");
+            graphClient.Connect();
+
+            List<PageNode> pns = new List<PageNode>()
             {
-                Console.WriteLine($"{p.Key}: {p.Value}");
+                new PageNode(1, "Page1", xml1),
+                new PageNode(2, "Page2", xml2),
+                new PageNode(3, "Page3", xml3),
+            };
+            using(client.Transactions.ITransaction trans = graphClient.BeginTransaction())
+            {
+                foreach(PageNode pn in pns)
+                    graphClient.Cypher.Create($"(n:Page {pn.LabelsAsString})").WithParams(pn).ExecuteWithoutResults();
+
+                trans.Commit();
             }
         }
     }
