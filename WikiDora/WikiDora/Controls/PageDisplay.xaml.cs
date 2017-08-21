9137d697cc413e35e2b5ccfb71b5cf06af6ae3cd
Laxdyn <dterry@student.neumont.edu>
2017-08-21 13:29:30 -0600

    Early GUI work trying to figure out wpf.

---
+++ WikiDora/WikiDora/Controls/PageDisplay.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WikiDora.Controls
+{
+    /// <summary>
+    /// Interaction logic for PageDisplay.xaml
+    /// </summary>
+    public partial class PageDisplay : UserControl
+    {
+        public PageDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLikePage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+        private void btnDislikePage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
